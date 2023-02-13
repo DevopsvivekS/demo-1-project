3a653c25d4ffd9fc1cfa0db2bc7b0a6747a579db
vivek <vivek@gmail.com>
2023-02-13 19:14:12 +0530

    this is second commit

---
+++ src/main/java/com/demo_1_company/demo_1_company/App.java
@@ -0,0 +1,13 @@
+package com.demo_1_company.demo_1_company;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hi this is vivek!" );
+    }
+}
---
+++ src/test/java/com/demo_1_company/demo_1_company/AppTest.java
@@ -0,0 +1,20 @@
+package com.demo_1_company.demo_1_company;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
